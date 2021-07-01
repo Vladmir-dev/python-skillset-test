0b6e6c86831682a1b7764f5fe81089494c4979b5
Vladmir-dev <tempestpius70@gmail.com>
2021-07-01 18:37:12 +0300

    put file close in while loop

--- client.py
+++ client.py
@@ -8,17 +8,17 @@ s.connect((socket.gethostname(), 1233))
 
 s.send(bytes("hello server", "utf-8"))
 
-with open('received_file', 'wb') as f:
-    print('file opened')
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print(data)
+f = open('received_file', 'wb')
+print('file opened')
+while True:
+    print('receiving data...')
+    data = s.recv(1024)
+    print(data)
 
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
+    if not data:
+        break
+    # write data to a file
+    f.write(data)
 
     f.close()
 
